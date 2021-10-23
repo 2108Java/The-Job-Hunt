319f4558abd85869e93971ea7fcabef9b5e09a20
Jihad Abdul-Latif <jihadabdullatif1@gmail.com>
2021-10-23 19:42:54 -0400

    The mods and daos

--- TheJobHuntJava/src/main/java/com/revature/repo/UserDao.java
+++ TheJobHuntJava/src/main/java/com/revature/repo/UserDao.java
@@ -1,5 +1,20 @@
 package com.revature.repo;
 
+import com.revature.models.User;
+
 public interface UserDao {
 
+	
+	/*
+	 * insert user
+update password
+update email
+﻿select user
+	 */
+
+	boolean insertUser(User u);
+	boolean updatePassword(User u);
+	boolean updateEmail(User u);
+	User selectUser(User u);
+	
 }
