3121f6de0939561042befc76e54aa482a87d3ae4
Jihad Abdul-Latif <jihadabdullatif1@gmail.com>
2021-10-23 20:27:21 -0400

    mods and daos

--- TheJobHuntJavaWithSpring/TheJobHunt/src/main/java/com/revature/models/User.java
+++ TheJobHuntJavaWithSpring/TheJobHunt/src/main/java/com/revature/models/User.java
@@ -1,5 +1,56 @@
 package com.revature.models;
 
-public class User {
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+	/*
+	 * create table users(
+	user_id serial primary key, --primary key _
+	user_email varchar(20) unique,
+	user_password varchar(20) not null
+	);
+	
+*/
+	@Id
+	@Column(name = "user_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column(name = "user_email", nullable = false, unique = true)
+	private String userEmail;
+	
+	@Column(name = "user_pwd", nullable = false, unique = false)
+	private String userPassword;
+	
+	@OneToMany(mappedBy ="users")
+	private List<Jobs> jobs;
+	
+	//@OneToOne
+	//List<Information> info;
+	
 }
+
--- TheJobHuntJavaWithSpring/TheJobHunt/src/main/java/com/revature/repo/InfoDao.java
+++ TheJobHuntJavaWithSpring/TheJobHunt/src/main/java/com/revature/repo/InfoDao.java
@@ -1,5 +1,48 @@
 package com.revature.repo;
 
-public interface InfoDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import com.revature.models.Information;
+import com.revature.models.User;
+
+public interface InfoDao extends JpaRepository<Information, Integer>{
+
+	
+/*
+ * insert info
+update info
+select info by id
+﻿delete info
+ */
+	//void update(Information i);
+	//users_info(user_info_id, first_name, last_name, street, city, state, zip, user_id)
+	
+	
+	@Modifying
+	@Query("update Information i set i.firstName = ?1 where i.users.id = ?2")
+	void updateFirstName(String firstName, int userid);
+	
+	@Modifying
+	@Query("update Information i set i.lastName = ?1 where i.users.id = ?2")
+	void updateLastName(String lastName, int userId);
+	
+	@Modifying
+	@Query("update Information i set i.street = ?1 where i.users.id  = ?2")
+	void updateStreet(String street, int userId);
+	
+	@Modifying
+	@Query("update Information i set i.city = ?1 where i.users.id  = ?2")
+	void updateCity(String city, int userId);
+		
+	@Modifying
+	@Query("update Information i set i.state = ?1 where i.users.id  = ?2")
+	void updateState(String state, int userId);
+	
+	@Modifying	
+	@Query("update Information i set i.zip = ?1 where i.users.id  = ?2")
+	void updateZipCode(int zipcode, int userId);
+
+	
 }
