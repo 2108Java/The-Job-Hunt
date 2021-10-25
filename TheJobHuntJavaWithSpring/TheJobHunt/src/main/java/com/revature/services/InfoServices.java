1bb5e9600b1796400159432c4aa1843cf39389e8
Tyler Nickl <tyler.a.nickl@gmail.com>
2021-10-25 04:08:11 -0700

    Adding InfoServices and InfoServicesImpl with methods

--- TheJobHuntJavaWithSpring/TheJobHunt/src/main/java/com/revature/services/InfoServices.java
+++ TheJobHuntJavaWithSpring/TheJobHunt/src/main/java/com/revature/services/InfoServices.java
@@ -1,5 +1,16 @@
 package com.revature.services;
 
+import com.revature.models.Information;
+
 public interface InfoServices {
 
+	
+	public boolean saveUserInfo(Information info);
+	public boolean updateFirstName(Information info);
+	public boolean updateLastName(Information info);
+	public boolean updateStreet(Information info);
+	public boolean updateCity(Information info);
+	public boolean updateState(Information info);
+	public boolean updateZipCode(Information info);
+	
 }
