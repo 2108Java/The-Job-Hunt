a0a4b0790499987836eee56b7562d28f0d7f766a
Jihad Abdul-Latif <jihadabdullatif1@gmail.com>
2021-10-26 17:59:51 -0400

    Transactionals

--- TheJobHuntJavaWithSpring/TheJobHunt/src/main/java/com/revature/repo/InfoDao.java
+++ TheJobHuntJavaWithSpring/TheJobHunt/src/main/java/com/revature/repo/InfoDao.java
@@ -4,11 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.revature.models.Information;
 import com.revature.models.User;
 
-@Repository
+@Repository("infoDao")
 public interface InfoDao extends JpaRepository<Information, Integer>{
 
 	
@@ -22,27 +23,32 @@ select info by id
 	//void update(Information i);
 	//users_info(user_info_id, first_name, last_name, street, city, state, zip, user_id)
 	
-	
+	@Transactional
 	@Modifying
 	@Query("update Information i set i.firstName = ?1 where i.users.id = ?2")
 	int updateFirstName(String firstName, int userid);
 	
+	@Transactional
 	@Modifying
 	@Query("update Information i set i.lastName = ?1 where i.users.id = ?2")
 	int updateLastName(String lastName, int userId);
 	
+	@Transactional
 	@Modifying
 	@Query("update Information i set i.street = ?1 where i.users.id  = ?2")
 	int updateStreet(String street, int userId);
 	
+	@Transactional
 	@Modifying
 	@Query("update Information i set i.city = ?1 where i.users.id  = ?2")
 	int updateCity(String city, int userId);
-		
+	
+	@Transactional
 	@Modifying
 	@Query("update Information i set i.state = ?1 where i.users.id  = ?2")
 	int updateState(String state, int userId);
 	
+	@Transactional
 	@Modifying	
 	@Query("update Information i set i.zip = ?1 where i.users.id  = ?2")
 	int updateZipCode(int zipcode, int userId);
--- TheJobHuntJavaWithSpring/TheJobHunt/src/main/java/com/revature/repo/SavedJobsDao.java
+++ TheJobHuntJavaWithSpring/TheJobHunt/src/main/java/com/revature/repo/SavedJobsDao.java
@@ -4,11 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.revature.models.Jobs;
 import com.revature.models.User;
 
-@Repository
+@Repository("savedJobDao")
 public interface SavedJobsDao extends JpaRepository<Jobs, Integer>{
 
 	/*
@@ -18,7 +19,7 @@ select saved job
 delete job
 	 */
 	
-	
+	@Transactional
 	@Modifying
 	@Query("update Jobs j set j.appliedFor = ?1 where j.id = ?2")
 	int updateAppliedFor(boolean applied, int id);
