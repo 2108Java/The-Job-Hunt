3851191e2cbbc7056add4a8271357ccd4a2c058c
Jihad Abdul-Latif <jihadabdullatif1@gmail.com>
2021-10-27 09:50:00 -0400

    lombok changes

--- TheJobHuntJavaWithSpring/TheJobHunt/src/main/java/com/revature/models/Information.java
+++ TheJobHuntJavaWithSpring/TheJobHunt/src/main/java/com/revature/models/Information.java
@@ -64,6 +64,75 @@ public class Information {
 	@Column(name = "zip", nullable = false, unique = false)
 	private int zip;
 
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public User getUsers() {
+		return users;
+	}
+
+	public void setUsers(User users) {
+		this.users = users;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	@Override
+	public String toString() {
+		return "Information [Id=" + Id + ", users=" + users + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", street=" + street + ", city=" + city + ", state=" + state + ", zip=" + zip + "]";
+	}
 	
 	
 }
