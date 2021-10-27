df6859f12c09b970e789c7ff693ef7327e4fbbd2
Tyler Nickl <tyler.a.nickl@gmail.com>
2021-10-27 06:52:47 -0700

    writing InfoController and InfoControllerImpl

--- TheJobHuntJavaWithSpring/TheJobHunt/src/main/java/com/revature/controllers/InfoController.java
+++ TheJobHuntJavaWithSpring/TheJobHunt/src/main/java/com/revature/controllers/InfoController.java
@@ -1,10 +1,15 @@
 package com.revature.controllers;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.revature.models.Information;
+import com.revature.models.User;
+
 public interface InfoController {
-	
-
-	public String testMethod();
-
+		
+	public Information getUserInfo(User user);
+	public Information changeSomeInfo(Map<String, String> newInfo);
+	public Information changeAllUserInfo(Map<String, String> newInfo);
 }
--- TheJobHuntJavaWithSpring/TheJobHunt/src/main/java/com/revature/controllers/InfoControllerImpl.java
+++ TheJobHuntJavaWithSpring/TheJobHunt/src/main/java/com/revature/controllers/InfoControllerImpl.java
@@ -1,18 +1,53 @@
 package com.revature.controllers;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("FoodController")
-@RequestMapping("/api/")
+import com.revature.models.Information;
+import com.revature.models.User;
+import com.revature.services.InfoServicesImpl;
+
+
+@RestController("InfoController")
+@RequestMapping(value = "/info")
 public class InfoControllerImpl implements InfoController {
 
+	
+	@Autowired
+	private InfoServicesImpl infoService;
+
+	
+
 	@Override
-	@GetMapping("/hello")
-	public String testMethod() {
-		// TODO Auto-generated method stub
-		return "Hello";
+	@GetMapping(value = "/info/{id}")	//@Path - necessary?
+	public Information getUserInfo(@RequestBody User user) { 
+		
+		return null;
 	}
 
+	
+	@Override
+	@PostMapping(value = "/info/{id}")
+	public Information changeAllUserInfo(@RequestBody Map<String,String> newInfo) { //do I need List<K,V> as arg datatype? or will it auto-marshal?
+
+		return null;
+	}
+
+	
+	@Override
+	@PutMapping(value = "/info/{id}")
+	public Information changeSomeInfo(@RequestBody Map<String,String> newInfo) {
+
+		return null;
+	}
+
+	
+	
 }
